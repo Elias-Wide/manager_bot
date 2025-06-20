f2f4c21045b7c22e1aa5e4ca1b748a3f60b98f98
Ilya Shirokov <ilshi2904@yandex.ru>
2025-06-20 17:03:01 +0300

    create points model

---
+++ app/points/models.py
@@ -0,0 +1,36 @@
+from typing import List
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+
+from app.core.database import Base
+from app.core.config import settings
+
+
+def same_as(column_name):
+    def default_function(context):
+        return context.current_parameters.get(column_name)
+
+    return default_function
+
+
+class Points(Base):
+    """Модель офиса.
+
+    Args:
+        id: id офиса в системе вб
+        addres: полный адрес
+        trades: созданные модели обмена кодами
+        managers: менеджеры офиса
+    """
+    id = Column(Integer, nullable=False, unique=True) 
+    addres = Column(String, nullable=False)
+    region_id = Column(
+        ForeignKey("regions.id", ondelete="SET NULL"), nullable=True
+    )
+    name = Column(String, nullable=True, default=same_as("addres"))
+    region = relationship("Regions", back_populates="points")
+    managers = relationship("Users", back_populates="points")
+
+    def __str__(self):
+        return f"{self.addres}"
--- app/users/models.py
+++ app/users/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import (
     BOOLEAN,
     BigInteger,
     Column,
+    Integer,
     String,
 )
 from sqlalchemy.orm import relationship
@@ -12,8 +13,12 @@ from app.core.database import Base
 class Users(Base):
 
     telegram_id = Column(BigInteger, unique=True, nullable=False)
+    phone_number = Column(Integer, nullable=False, unique=True)
     username = Column(String, nullable=False)
+    manager_id = Column(BigInteger, unique=True, nullable=False)
+    is_region_admin = Column(BOOLEAN, default=False) 
     ban = Column(BOOLEAN, default=False)
+    point_id = Column(Integer)
 
     def __str__(self):
         return f"User @{self.username}"
